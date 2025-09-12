5b0bad680e6b178303dc3222d5cc832c86beae24
vominh-source <vominh272004@gmail.com>
2025-09-13 01:09:40 +0700

    Add UI for Sign In and Sign Up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
+import SignInPage from '@/pages/SignInPage';
 import AddRecipePage from './pages/add-recipe';
 import HomeLayout from './layouts/home-layout';
-import { ThemeProvider } from "@mui/material/styles";
-import { muiTheme } from "@/lib/muiTheme";
-import LandingPage from "@/pages/LandingPage";
+import { ThemeProvider } from '@mui/material/styles';
+import { muiTheme } from '@/lib/muiTheme';
+import LandingPage from '@/pages/LandingPage';
 import './App.css';
 import ProfilePage from '@/pages/Profile';
-import ExploreRecipes from "@/pages/ExploreRecipes";
+import ExploreRecipes from '@/pages/ExploreRecipes';
 import RecipeDetailPage from '@/pages/RecipeDetailPage';
+import SignUpPage from '@/pages/SignUpPage';
 
 function App() {
   return (
-     <ThemeProvider theme={muiTheme}>
-    <Routes>
-      <Route element={<HomeLayout />}>
-        <Route index element={<LandingPage />} />
-        <Route path="/explore" element={<ExploreRecipes />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/add-recipe" element={<AddRecipePage />} />
-        <Route path="/recipes/:id" element={<RecipeDetailPage />} />
-      </Route>
-    </Routes>
+    <ThemeProvider theme={muiTheme}>
+      <Routes>
+        <Route element={<HomeLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="/explore" element={<ExploreRecipes />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/add-recipe" element={<AddRecipePage />} />
+          <Route path="/recipes/:id" element={<RecipeDetailPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+        </Route>
+      </Routes>
     </ThemeProvider>
-
   );
 }
 
